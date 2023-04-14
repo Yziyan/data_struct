aa3ae990e12d77fc5a8c32c744bd46efaf74e622
Ciusyan <1533460130@qq.com>
2023-04-14 18:48:44 +0800

    相交链表

---
+++ 15_算法/src/run/ciusyan/链表/_160_相交链表.java
@@ -0,0 +1,19 @@
+package run.ciusyan.链表;
+
+/**
+ * https://leetcode.cn/problems/intersection-of-two-linked-lists/
+ */
+public class _160_相交链表 {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) return null;
+        ListNode curA = headA;
+        ListNode curB = headB;
+
+        while (curA != curB) {
+            curA = (curA == null) ? headB : curA.next;
+            curB = (curB == null) ? headA : curB.next;
+        }
+
+        return curA;
+    }
+}
