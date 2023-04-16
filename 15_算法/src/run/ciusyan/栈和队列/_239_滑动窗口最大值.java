39949a4b210315af49e248c616613e206da46590
Ciusyan <1533460130@qq.com>
2023-04-16 18:09:31 +0800

    滑动窗口的另一种实现

--- 15_算法/src/run/ciusyan/栈和队列/_239_滑动窗口最大值.java
+++ 15_算法/src/run/ciusyan/栈和队列/_239_滑动窗口最大值.java
@@ -7,7 +7,54 @@ import java.util.LinkedList;
  * https://leetcode.cn/problems/sliding-window-maximum/
  */
 public class _239_滑动窗口最大值 {
+
+    /**
+     * 更简单的思路实现
+     */
     public int[] maxSlidingWindow(int[] nums, int k) {
+        if (nums == null || nums.length == 0 || k <= 0) return new int[0];
+        if (k == 1) return nums;
+
+        // nums.length - (k + 1) 是滑动窗口的个数
+        int[] maxes = new int[nums.length - k + 1];
+
+        // 有效的最大值索引
+        int maxIndex = 0;
+        // 找出第一个窗口内的最大值索引
+        for (int i = 1; i < k; i++) {
+            if (nums[i] >= nums[maxIndex]) maxIndex = i;
+        }
+
+        maxes[0] = nums[maxIndex];
+
+        // 开始滑动比较 （li是每一个窗口的第一个索引）
+        for (int li = 1; li < maxes.length; li++) {
+            int ri = li + k - 1; // ri 是每一个窗口的最后一个索引
+
+            if (maxIndex < li) {
+                // 说明最大值索引不合法了，需要找出这一个窗口内部的最大值索引
+                maxIndex = li;
+                // 求出 [li, ri] 的最大值索引
+                for (int i = li + 1; i <= ri; i++) {
+                    if (nums[i] >= nums[maxIndex]) maxIndex = i;
+                }
+
+            } else if (nums[ri] >= nums[maxIndex]) {
+                // 说明最大值索引合法，将其与新滑入的元素比较，取大的那个
+                maxIndex = ri;
+            }
+
+            // 来到这里，说明最大值在 maxIndex那，给结果复制
+            maxes[li] = nums[maxIndex];
+        }
+
+        return maxes;
+    }
+
+    /**
+     * 利用单调队列实现（双端队列）
+     */
+    public int[] maxSlidingWindow1(int[] nums, int k) {
         if (nums == null || nums.length == 0 || k <= 0) return new int[0];
         if (k == 1) return nums;
 
