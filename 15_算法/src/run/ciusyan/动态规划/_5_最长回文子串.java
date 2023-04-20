f578a8356157c7e110d31dc92dff4eb3106dde10
Ciusyan <1533460130@qq.com>
2023-04-20 14:35:10 +0800

    扩展中心法  1

--- 15_算法/src/run/ciusyan/动态规划/_5_最长回文子串.java
+++ 15_算法/src/run/ciusyan/动态规划/_5_最长回文子串.java
@@ -6,13 +6,66 @@ package run.ciusyan.动态规划;
  */
 public class _5_最长回文子串 {
 
+    /**
+     * 扩展中心法 1
+     */
+    public static String longestPalindrome(String s) {
+        if (s == null) return null;
+        char[] chars = s.toCharArray();
+        if (chars.length <= 1) return s;
+
+        // 默认起始位置
+        int begin = 0;
+        // 默认最长回文子串的长度
+        int maxLen = 1;
+
+        // 从第一个元素没必要扩展、最后一个元素也没必要扩展
+        for (int i = 1; i < chars.length - 1; i++) {
+            // 此次扩展的回文子串最大长度
+            int len = Math.max(
+                palindromeLen(chars, i - 1, i), // 偶数个，从缝隙开始扩展
+                palindromeLen(chars, i - 1, i + 1) // 奇数个，直接从元素开始扩展
+            );
+
+            // 找到最大长度了，看看有没有比原先还长
+            if (len > maxLen) {
+                // 说明比原先还长，更新起始索引、最大长度
+                // 起始位置 = 开始扩展位置 - 扩展后的长度 / 2
+                begin = i - (len >> 1);
+
+                maxLen = len;
+            }
+        }
+        // 但是上面，还有最后一个空隙没有扩展
+        if (chars[chars.length - 1] == chars[chars.length - 2] && maxLen < 2) {
+            // 最后一个空隙，扩展后最大的距离也只能是 2，
+
+            // 表明上面扩展后，都没有第一个空隙这么长，
+            begin = chars.length - 2;
+            maxLen = 2;
+        }
+
+        return new String(chars, begin, maxLen);
+    }
+
+    /**
+     * 求出 chars 中，从 l 和 r 开始，能向左和向右扩展的最大长度
+     */
+    private static int palindromeLen(char[] chars, int l, int r) {
+        // 这个说明，右边不会越界 && 左边不会越界 && l 、r 位置的字符相等
+        while (r < chars.length && l >= 0 && chars[l] == chars[r]) {
+            l--;
+            r++;
+        }
+
+        // 来到这里，要么是右边越界了，要么是左边越界了，要么是扩展到不相等的字符了，返回最终的长度
+        return r - l - 1;
+    }
 
     /**
      * 动态规划 + 一维数组
-     * @param s
-     * @return
      */
-    public static String longestPalindrome(String s) {
+    public static String longestPalindrome3(String s) {
         if (s == null) return null;
         char[] chars = s.toCharArray();
         if (chars.length <= 1) return s;
@@ -140,6 +193,7 @@ public class _5_最长回文子串 {
     }
 
     public static void main(String[] args) {
-        System.out.println(longestPalindrome("ccc"));
+        System.out.println(longestPalindrome("cbbd"));
+        System.out.println(longestPalindrome("babad"));
     }
 }
